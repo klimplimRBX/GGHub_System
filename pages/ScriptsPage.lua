1d295984c9b3706d937985b70946ccdfde3878f4
klimplimRBX <klimplimnimiriumtuto@gmail.com>
2026-02-25 19:51:14 -0300

    Update ScriptsPage.lua

--- pages/ScriptsPage.lua
+++ pages/ScriptsPage.lua
@@ -25,15 +25,15 @@ return function(ctx)
 				if v < 100 then
 					speed = v
 				elseif v < 200 then
-					speed = v * 1.05
-				elseif v < 300 then
 					speed = v * 1.10
-				elseif v < 400 then
-					speed = v * 1.15
-				elseif v < 500 then
-					speed = v * 1.20
-				else
+				elseif v < 300 then
 					speed = v * 1.25
+				elseif v < 400 then
+					speed = v * 1.50
+				elseif v < 500 then
+					speed = v * 1.70
+				else
+					speed = v * 1.10
 				end
 			end
 		end)
@@ -1152,3 +1152,4 @@ return function(ctx)
 	note.TextYAlignment = Enum.TextYAlignment.Top
 	note.Parent = scriptPage
 end
+
